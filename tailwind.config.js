be1ae8a4896dfe3341f4234a74ab6f00794df233
AJ Webb <webbushka@gmail.com>
2023-07-07 14:35:17 -1000

    Refresh the wallets page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './docs/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{md,mdx,js,jsx,ts,tsx}',
+    './docs/**/*.{md,mdx,js,jsx,ts,tsx}',
+  ],
   corePlugins: { preflight: false },
   theme: {
     extend: {},
